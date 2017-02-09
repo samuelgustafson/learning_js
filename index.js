b9cc35b3b53fbe8ed2c115ed75ed752e6ad5a238
samuelgustafson <samelgw@gmail.com>
2017-02-08 20:45:23 -0500

    learned how to loop over collections using a standard for loop as well as a forEach loop that is only available on the array data type

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var server = express();
 var port = 8080;
-
+//this line is wiring up a sever power called express.static it allows the server to serve static files from a directory
  server.use(express.static(__dirname + '/public'));
 
  server.listen(port, function(){
